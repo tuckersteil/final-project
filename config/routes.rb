57aafcfb30228714c6a9cbb31a8ad72eca8b2b99
Tucker Steil <tuckersteil@gmail.com>
2023-02-27 14:54:56 -0800

    set up get root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "trainers#index"
-  get "/", to: "trainers#index"
+  root "trainers#index"
+
 end
