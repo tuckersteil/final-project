f0a2498d5404dbf14996f8bc1a22054671adab29
Tucker Steil <tuckersteil@gmail.com>
2023-06-06 14:16:24 -0700

    Adding API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get "/me", to: "users#show"
-  post "/login", to: "sessions#create"
-  post "/signup", to: "users#create"
-  get "/trainers/:activity/:location", to: "trainer_activities#list"
-  get "/timey/:train", to: "trainers#timey"
-  get "confirm/:id", to: "trainer_activities#confirm"
+  namespace :api do
+    get "/me", to: "users#show"
+    post "/login", to: "sessions#create"
+    post "/signup", to: "users#create"
+    get "/trainers/:activity/:location", to: "trainer_activities#list"
+    get "/timey/:train", to: "trainers#timey"
+    get "confirm/:id", to: "trainer_activities#confirm"
     get "confirmy/:id", to: "activities#confirmy"
     get "/users", to: "users#index"
     delete "/logout", to: "sessions#destroy"
@@ -12,14 +13,32 @@ Rails.application.routes.draw do
     patch "/trainers/:name/:time", to: "trainers#remove"
     patch "/removey/:name/:time", to: "trainers#removey"
     resources :trainers
-  resources :bookings
-  
-   
+    resources :bookings 
     resources :users 
     resources :bookings
     resources :trainer_activities
     resources :activities
     resources :trainers
+  end 
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  post "/signup", to: "users#create"
+  get "/trainers/:activity/:location", to: "trainer_activities#list"
+  get "/timey/:train", to: "trainers#timey"
+  get "confirm/:id", to: "trainer_activities#confirm"
+  get "confirmy/:id", to: "activities#confirmy"
+  get "/users", to: "users#index"
+  delete "/logout", to: "sessions#destroy"
+  get "/booksesh", to: "users#userbooking"
+  patch "/trainers/:name/:time", to: "trainers#remove"
+  patch "/removey/:name/:time", to: "trainers#removey"
+  resources :trainers
+  resources :bookings 
+  resources :users 
+  resources :bookings
+  resources :trainer_activities
+  resources :activities
+  resources :trainers
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
