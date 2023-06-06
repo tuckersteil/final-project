aedf22cff0cb68912107bd82cf8a948f09c2f9d2
Tucker Steil <tuckersteil@gmail.com>
2023-06-06 14:53:34 -0700

    moved mailer to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,4 +70,13 @@ Rails.application.configure do
 
 
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => "tuckersteil@gmail.com",
+  :password             => "iwbejcwlalkigexp",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
 end
